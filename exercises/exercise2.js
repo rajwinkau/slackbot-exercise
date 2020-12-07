91564099d2a042e50710df14e32342c6ef6cf06d
Ken McGrady <ken.mcgrady@gmail.com>
2020-12-07 15:15:33 -0800

    Add exercise2

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -2,6 +2,30 @@
 module.exports.ACTIVATE_BOT = false;
 
 module.exports.botScripts = [
-  // Write an interaction where
+  // Write an interaction where one user says "I'm tired", the bot says "Wake up!"
   //
+  // mychatbot> I'm tired
+  // mychatbot responds "Wake up!"
+  
+
+
+
+
+  // Write an interaction where one user asks the bot 
+  // "What's the area of a circle with radius X?" where X is any number
+  // The chatbot will respond to the user with the answer.
+  // 
+  // mychatbot> mychatbot What's the area of a circle with radius 5?
+  // mychatbot responds to user "The area is 78.5398...."
+  
+
+
+
+
+  // Write an interaction where one user asks the bot 
+  // "Tell me a story with NAME1 and NAME2" where NAME1 and NAME2 is any string
+  // The chatbot will respond with the answer.
+  // 
+  // mychatbot> mychatbot Tell me a story with Jack and Jill
+  // mychatbot responds to user "Jack and Jill went up the hill to fetch a pail of water."
 ];
